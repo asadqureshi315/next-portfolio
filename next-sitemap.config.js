117ed1ab2c523a5952d29e27c8842a8c3f7707fb
Asad Qureshi <asaddullah111@gmail.com>
2025-03-27 00:42:14 +0530

    removed contact page, changes to sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   siteUrl: "https://asadqureshi.tech",
-  generateRobotsTxt: true,
-  exclude: ["/panel", "/login"],
+  generateRobotsTxt: true, // Automatically generate robots.txt
+  sitemapSize: 5000, // Avoid multiple sitemap files
+  exclude: ["/panel", "/login"], // Exclude private pages
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/panel", "/login"], // Block these in robots.txt
+      },
+    ],
+  },
 };
--- src/app/(public pages)/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-  return (
-    <div>
-      <h1 className=" text-white">Contacts</h1>
-    </div>
-  );
-}
